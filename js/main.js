86bdcf146016a8dfe5a48dc4b70be3979a25c016
reemhaitham12 <reemhaitham69@gmail.com>
2024-07-14 16:42:15 +0300

    add Area and Intgredients

--- js/main.js
+++ js/main.js
@@ -77,6 +77,7 @@ let searchBox = document.getElementById("searchBox");
 // ! start show search page
 function ShowSearch() {
     CloseNavbar();
+    searchBox.classList.remove('d-none');
     searchBox.innerHTML = `
     <div class="row g-3">
                 <div class="col-lg-6">
@@ -99,7 +100,6 @@ async function SearchName(word) {
     Row.innerHTML = '';
     const responseApi = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${word}`)
     const response = await responseApi.json();
-    console.log(response);
     if (response.meals) {
         ShowFood(response.meals);
     } else {
@@ -125,31 +125,31 @@ async function SearchLetter(letter) {
 // ! start Category
 async function Category() {
     CloseNavbar();
-    Row.innerHTML=""
-    searchBox.innerHTML=""
+    Row.innerHTML = ""
+    searchBox.innerHTML = ""
     let responseApi = await fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`);
     let response = await responseApi.json();
     // console.log(response)
-    ShowCategory(response.categories);
+    ShowCategory(response);
 }
 
 // ! end Category
 
 // ! start Show Category
-function ShowCategory(card) {
+function ShowCategory(response) {
     let cartona = ``
-    for (let i = 0; i < card.length; i++) {
-        let shortDescription = card[i].strCategoryDescription.split(" ").slice(0, 20).join(" ");
+    for (let i = 0; i < response.categories.length; i++) {
+        let shortDescription = response.categories[i].strCategoryDescription.split(" ").slice(0, 30).join(" ");
         cartona += `
         <div class="col-lg-3">
                     <div
                         class="card position-relative overflow-hidden rounded-3">
                         <div class="image">
-                            <img src="${card[i].strCategoryThumb}" alt=""
+                            <img src="${response.categories[i].strCategoryThumb}" alt=""
                                 class="w-100 d-block">
                         </div>
                         <div class="titleCategory">
-                            <h3 class="text-center">${card[i].strCategory}</h3>
+                            <h3 class="text-center">${response.categories[i].strCategory}</h3>
                             <div class="description">
                                 <p>${shortDescription}</p>
                             </div>
@@ -158,10 +158,72 @@ function ShowCategory(card) {
                 </div>
         `
     }
-    Row.innerHTML=cartona;
+    Row.innerHTML = cartona;
 }
 
 // ! end Show Category
+
+// ! Start Area
+async function Area() {
+    CloseNavbar();
+    Row.innerHTML = "";
+    // searchBox.innerHTML = "";
+    searchBox.classList.add('d-none');
+    const responseApi = await fetch(`https://www.themealdb.com/api/json/v1/1/list.php?a=list`);
+    const response = await responseApi.json();
+    ShowArea(response);
+}
+// ! end Area
+
+// ! start ShowArea
+function ShowArea(response) {
+    let cartona = ``
+    for (let i = 0; i < response.meals.length; i++) {
+        cartona += `
+        <div class="col-lg-3">
+                    <div class="home">
+                        <i class="fa-solid fa-house-laptop"></i>
+                        <h3>${response.meals[i].strArea}</h3>
+                    </div>
+                </div>
+        `
+    }
+    Row.innerHTML = cartona;
+}
+// ! end ShowArea
+
+// ! start Ingredients
+async function Ingredients() {
+    CloseNavbar();
+    Row.innerHTML = "";
+    searchBox.innerHTML = "";
+    const responseApi = await fetch(`https://www.themealdb.com/api/json/v1/1/list.php?i=list`);
+    const response = await responseApi.json();
+    ShowIngredients(response);
+}
+// ! end Ingredients
+
+// ! start Show Ingredients
+function ShowIngredients(response) {
+    let cartona = ``;
+    for (let i = 0; i < response.meals.length; i++) {
+        if (response.meals[i].strDescription) {
+            let shortDescription = response.meals[i].strDescription.split(" ").slice(0, 30).join(" ");
+            cartona += `
+            <div class="col-lg-3">
+                <div class="cards">
+                    <i class="fa-solid fa-drumstick-bite"></i>
+                    <h3 class="text-capitalize">${response.meals[i].strIngredient}</h3>
+                    <p>${shortDescription}</p>
+                </div>
+            </div>
+            `;
+        }
+    }
+    Row.innerHTML = cartona;
+}
+// ! end Show Ingredients
+
 
 
 
