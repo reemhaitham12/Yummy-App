dd31d3c2e6d2a5190edbe082c196667567b7d6b1
reemhaitham12 <reemhaitham69@gmail.com>
2024-07-15 04:38:28 +0300

    Finish Yummy

--- js/main.js
+++ js/main.js
@@ -2,30 +2,21 @@
 const loading = document.querySelector(".loading");
 const loadering = document.querySelector(".Sppiner");
 //! if you click on image icon ,the website will reload or refresh another food random 
-
 // ! random food
-// document.addEventListener("DOMContentLoaded", function () {
-//     getRandomMeals(20);
-// });
-// window.onload = function() {
-//     getRandomMeals(20);
-// };
-window.onload = async function() {
-    // إظهار عنصر التحميل
+window.onload = async function () {
+    CloseNavbar();
     loading.classList.remove('d-none');
-
     try {
-        // جلب 20 وجبة عشوائية
         await getRandomMeals(20);
     } catch (error) {
         console.error('Error fetching random meals on page load:', error);
-    } finally {
-        // إخفاء عنصر التحميل بعد الانتهاء من جلب الوجبات
-        loading.classList.add('d-none');
     }
+    loading.classList.add('d-none');
 };
+
 // ! using link api random meal
 async function getRandomMeals(count) {
+    CloseNavbar();
     try {
         searchBox.classList.add('d-none');
         Row.classList.add('d-none');
@@ -41,7 +32,6 @@ async function getRandomMeals(count) {
                 console.error('No meal found');
             }
         }
-
         ShowFood(Array.from(meals));
         loadering.classList.add('d-none');
     } catch (error) {
@@ -49,10 +39,6 @@ async function getRandomMeals(count) {
         loadering.classList.add('d-none');
     }
 }
-
-
-
-
 
 // ! navbar open and close //
 
@@ -198,19 +184,16 @@ async function MealId(FoodID) {
 
 // ! End MealId
 
-
-
-
 // ! start Category
 async function Category() {
     CloseNavbar();
     Row.classList.add('d-none');
-    loading.classList.remove('d-none');
+    loadering.classList.remove('d-none');
     searchBox.classList.add('d-none');
     let responseApi = await fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`);
     let response = await responseApi.json();
     ShowCategory(response.categories);
-    loading.classList.add('d-none');
+    loadering.classList.add('d-none');
 }
 
 // ! end Category
@@ -248,11 +231,11 @@ function ShowCategory(card) {
 async function FilterCategory(Category) {
     Row.classList.add("d-none");
     //  ! loading
-    loading.classList.remove('d-none');
+    loadering.classList.remove('d-none');
     const responseApi = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${Category}`);
     const response = await responseApi.json();
     ShowFood(response.meals);
-    loading.classList.add('d-none');
+    loadering.classList.add('d-none');
 }
 
 // ? ---------------------------------------- ?//
@@ -262,13 +245,13 @@ async function Area() {
     CloseNavbar();
     Row.classList.add('d-none');
     // ! loading
-    loading.classList.remove('d-none');
+    loadering.classList.remove('d-none');
     searchBox.classList.add('d-none');
     const responseApi = await fetch(`https://www.themealdb.com/api/json/v1/1/list.php?a=list`);
     const response = await responseApi.json();
     ShowArea(response.meals);
     // ! loading
-    loading.classList.add('d-none');
+    loadering.classList.add('d-none');
 }
 // ! end Area
 
@@ -293,12 +276,11 @@ function ShowArea(card) {
 // ! Filter by Area
 async function FilterArea(Area) {
     Row.classList.add("d-none");
-    // ! loading
-    loading.classList.remove('d-none');
+    loadering.classList.remove('d-none');
     const responseApi = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${Area}`);
     const response = await responseApi.json();
     ShowFood(response.meals[0]);
-    loading.classList.add('d-none');
+    loadering.classList.add('d-none');
 }
 // ? ---------------------------------------- ?//
 
@@ -307,13 +289,13 @@ async function Ingredients() {
     CloseNavbar();
     Row.classList.add('d-none');
     // ! loading
-    loading.classList.remove('d-none');
+    loadering.classList.remove('d-none');
     searchBox.classList.add('d-none');
     const responseApi = await fetch(`https://www.themealdb.com/api/json/v1/1/list.php?i=list`);
     const response = await responseApi.json();
     ShowIngredients(response.meals);
     // ! loading
-    loading.classList.add('d-none');
+    loadering.classList.add('d-none');
 }
 // ! end Ingredients
 
@@ -343,12 +325,12 @@ function ShowIngredients(card) {
 async function FilterIngredients(Ingredients) {
     Row.classList.add("d-none");
     // ! loading
-    loading.classList.remove('d-none');
+    loadering.classList.remove('d-none');
     const responseApi = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${Ingredients}`);
     const response = await responseApi.json();
     ShowFood(response.meals);
     // ! loading
-    loading.classList.add('d-none');
+    loadering.classList.add('d-none');
 }
 
 // ? ---------------------------------------- ?//
@@ -420,14 +402,247 @@ function ShowDetails(food) {
 
 
 
+// ! contact us
+function ShowContact() {
+    CloseNavbar();
+    Row.classList.remove('d-none');
+    searchBox.classList.remove('d-none');
+    loadering.classList.remove('d-none');
+    let cartona = `
+    <div class="contact">
+        <div class="row g-3">
+            <div class="col-lg-6">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" id="nameInput"
+                        placeholder="Enter Your Name" onkeyup="validationName(this)">
+                </div>
+                <ul class="alert alert-danger p-0 invalid-feedback">
+                    <li>Name Is Required</li>
+                    <li>Minimum Length 2 and Maximum 20</li>
+                </ul>
+            </div>
+            <div class="col-lg-6">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" id="phoneInput"
+                        placeholder="Enter Your Phone" onkeyup="validationPhone(this)">
+                    <ul class="alert alert-danger p-0 invalid-feedback">
+                        <li>Phone Is Required</li>
+                        <li>Must have 11 numbers</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="input-group mb-3">
+                    <input type="password" class="form-control" id="passwordInput"
+                        placeholder="Enter Your Password" onkeyup="validationPassword(this)">
+                    <ul class="alert alert-danger p-0 invalid-feedback">
+                        <li>Password Is Required</li>
+                        <li>Minimum eight characters, at least one letter and one number</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" id="emailInput"
+                        placeholder="Enter Your Email" onkeyup="validationEmail(this)">
+                    <ul class="alert alert-danger p-0 invalid-feedback">
+                        <li>Email Is Required</li>
+                        <li>Invalid Email Format</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" id="ageInput"
+                        placeholder="Enter Your Age" onkeyup="validationAge(this)">
+                    <ul class="alert alert-danger p-0 invalid-feedback">
+                        <li>Age Is Required</li>
+                        <li>Minimum 10, Maximum 80</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="input-group mb-3">
+                    <input type="password" class="form-control" id="repasswordInput"
+                        placeholder="Re-enter Password" onkeyup="validateRePassword(this, document.querySelector('#passwordInput'))">
+                    <ul class="alert alert-danger p-0 invalid-feedback">
+                        <li>Password Is Required</li>
+                        <li>Minimum eight characters, at least one letter and one number</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="d-flex justify-content-center mt-3">
+            <button class="btn btn-outline-danger" id="submitBtn" disabled>Submit</button>
+        </div>
+    </div>
+    `;
+    Row.innerHTML = cartona;
+    loadering.classList.add('d-none');
+    const AllInput = document.querySelectorAll(".form-control");
+    AllInput.forEach(input => {
+        input.addEventListener('input', function() {
+            validateForm(AllInput);
+        });
+    });
+    const btnSubmit = document.getElementById("submitBtn");
+    btnSubmit.addEventListener('click', function (e) {
+        e.preventDefault();
+        BtnSubmit();
+    });
+}
+
+function validateForm(inputs) {
+    let isValid = true;
+    inputs.forEach(input => {
+        switch (input.getAttribute('placeholder')) {
+            case 'Enter Your Name':
+                if (!validationName(input)) isValid = false;
+                break;
+            case 'Enter Your Phone':
+                if (!validationPhone(input)) isValid = false;
+                break;
+            case 'Enter Your Password':
+                if (!validationPassword(input)) isValid = false;
+                break;
+            case 'Enter Your Email':
+                if (!validationEmail(input)) isValid = false;
+                break;
+            case 'Enter Your Age':
+                if (!validationAge(input)) isValid = false;
+                break;
+            case 'Re-enter Password':
+                if (!validateRePassword(input, document.querySelector('#passwordInput'))) isValid = false;
+                break;
+        }
+    });
+
+    const btnSubmit = document.getElementById("submitBtn");
+    if (isValid) {
+        btnSubmit.removeAttribute('disabled');
+    } else {
+        btnSubmit.setAttribute('disabled', 'disabled');
+    }
+
+    return isValid;
+}
+
+function  BtnSubmit() {
+    const formData = {};
+    const inputs = document.querySelectorAll(".form-control");
+    inputs.forEach(input => {
+        formData[input.id] = input.value;
+    });
+    localStorage.setItem("formData", JSON.stringify(formData));
+    inputs.forEach(input => {
+        input.value = "";
+    });
+    window.location.reload();
+    window.onload = async function () {
+        CloseNavbar();
+        loading.classList.remove('d-none');
+        try {
+            await getRandomMeals(20);
+        } catch (error) {
+            console.error('Error fetching random meals on page load:', error);
+        }
+        loading.classList.add('d-none');
+    };
+}
 
 
+function validationName(input) {
+    const value = input.value.trim();
+    const regex = /^[a-zA-Z\u00C0-\u017F'\s]{2,20}$/; // Letters and spaces, 2 to 20 characters
+    const valid = regex.test(value);
 
+    if (valid) {
+        input.classList.remove('is-invalid');
+        input.classList.add('is-valid');
+    } else {
+        input.classList.remove('is-valid');
+        input.classList.add('is-invalid');
+    }
 
+    return valid;
+}
 
+function validationPhone(input) {
+    const value = input.value.trim();
+    const regex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/; // Phone number format
+    const valid = regex.test(value);
 
+    if (valid) {
+        input.classList.remove('is-invalid');
+        input.classList.add('is-valid');
+    } else {
+        input.classList.remove('is-valid');
+        input.classList.add('is-invalid');
+    }
 
+    return valid;
+}
 
+function validationPassword(input) {
+    const value = input.value.trim();
+    const regex = /^(?=.*[a-zA-Z])(?=.*\d)[A-Za-z\d!#$%&?^*@~() "]{8,}$/; // Minimum eight characters, at least one letter and one number
+    const valid = regex.test(value);
 
+    if (valid) {
+        input.classList.remove('is-invalid');
+        input.classList.add('is-valid');
+    } else {
+        input.classList.remove('is-valid');
+        input.classList.add('is-invalid');
+    }
 
+    return valid;
+}
 
+function validationEmail(input) {
+    const value = input.value.trim();
+    const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // Email format
+    const valid = regex.test(value);
+
+    if (valid) {
+        input.classList.remove('is-invalid');
+        input.classList.add('is-valid');
+    } else {
+        input.classList.remove('is-valid');
+        input.classList.add('is-invalid');
+    }
+
+    return valid;
+}
+
+function validationAge(input) {
+    const value = input.value.trim();
+    const regex = /^(1[0-9]|[2-7][0-9]|80)$/; // Age between 10 and 80
+    const valid = regex.test(value);
+
+    if (valid) {
+        input.classList.remove('is-invalid');
+        input.classList.add('is-valid');
+    } else {
+        input.classList.remove('is-valid');
+        input.classList.add('is-invalid');
+    }
+
+    return valid;
+}
+
+function validateRePassword(input, passwordInput) {
+    const value = input.value.trim();
+    const password = passwordInput.value.trim();
+    const valid = value === password;
+
+    if (valid) {
+        input.classList.remove('is-invalid');
+        input.classList.add('is-valid');
+    } else {
+        input.classList.remove('is-valid');
+        input.classList.add('is-invalid');
+    }
+
+    return valid;
+}
